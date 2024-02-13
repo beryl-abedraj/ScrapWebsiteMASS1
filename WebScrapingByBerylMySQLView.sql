83b86c1340f93b69927b706243d58efd65edbf1e
Beryl Abed Raj S <157201143+beryl-abedraj@users.noreply.github.com>
2024-02-13 23:43:04 +0530

    Update WebScrapingByBerylMySQLView.sql

--- WebScrapingByBerylMySQLView.sql
+++ WebScrapingByBerylMySQLView.sql
@@ -1,4 +1,3 @@
 use python_db;
-#create table series_list_25( id int primary key auto_increment , rankings int , series_name varchar(50) , started_year varchar(10), ratings int , lead_actor_name varchar(50)) auto_increment = 100;
-#drop table series_list_25
+create table series_list_25( id int primary key auto_increment , rankings int , series_name varchar(50) , started_year varchar(10), ratings int , lead_actor_name varchar(50)) auto_increment = 100;
 select * from series_list_25;
